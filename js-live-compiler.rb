25402b590f971995e90920ea7679bfe6b87b329b
Drew Neil <andrew.jr.neil@gmail.com>
2011-03-23 18:13:56 +0200

    Make live compiler output time of change, and duration of compilation.

--- js-live-compiler.rb
+++ js-live-compiler.rb
@@ -1,6 +1,9 @@
 watch('public/app/.*\.js') do |md|
-  puts "#{md} was changed. Compiling..."
-  system "java -jar closure-compiler/compiler.jar #{build_input_file_list} --js_output_file public/javascripts/app.min.js"
+  puts "#{md} was changed at #{start = Time.now}"
+
+  command = `java -jar closure-compiler/compiler.jar #{build_input_file_list} --js_output_file public/javascripts/app.min.js`
+  command = "Compilation completed in #{sprintf('%.2f', Time.now - start)}s\n\n" if command.empty?
+  puts command
 end
 
 def all_js_resources
