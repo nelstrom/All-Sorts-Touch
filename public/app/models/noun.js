00d819a71ed92729509d0ecfdee0f19577794d13
Drew Neil <andrew.jr.neil@gmail.com>
2011-03-24 09:55:42 +0200

    Handle an inline seed object from noun json.

--- public/app/models/noun.js
+++ public/app/models/noun.js
@@ -1,7 +1,7 @@
 Ext.regModel('Noun', {
     fields: [
-        {name: 'plural', type: 'string'},
-        {name: 'slug', type: 'string'},
-        {name: 'occurences', type: 'number'}
+        { name: 'plural',      type: 'string' },
+        { name: 'slug',        type: 'string' },
+        { name: 'occurences',  type: 'number' }
     ]
 });
--- public/app/models/suggestion.js
+++ public/app/models/suggestion.js
@@ -1,12 +1,11 @@
 Ext.regModel('Suggestion', {
     fields: [
-        {name: 'article', type: 'string'},
-        {name: 'noun', type: 'string'},
-        {name: 'noun_slug', type: 'string'},
-        {name: 'collector', type: 'string'},
-        {name: 'collector_slug', type: 'string'},
-        {name: 'seed_tweet_id', type: 'string'},
-        {name: 'seed_user_name', type: 'string'},
-        {name: 'vote_count', type: 'number'}
+        { name: 'article',         type: 'string' },
+        { name: 'noun',            type: 'string' },
+        { name: 'noun_slug',       type: 'string' },
+        { name: 'collector',       type: 'string' },
+        { name: 'collector_slug',  type: 'string' },
+        { name: 'vote_count',      type: 'number' },
+        { name: 'seed' }
     ]
 });
