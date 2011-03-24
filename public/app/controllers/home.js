fa26bc8d052b826b9c926fc0e738157b285c6010
Drew Neil <andrew.jr.neil@gmail.com>
2011-03-24 15:24:01 +0200

    Only load home/popular datastores when no data.

--- public/app/controllers/home.js
+++ public/app/controllers/home.js
@@ -2,12 +2,16 @@ Ext.regController("home", {
     model: "Suggestion",
     
     index: function() {
-        App.home_store.load();
+        if (App.home_store.data.length === 0) {
+            App.home_store.load();
+        }
         App.views.viewport.setActiveItem(App.views.homecard);
     },
     
     popular: function() {
-        App.popular_store.load();
+        if (App.popular_store.data.length === 0) {
+            App.popular_store.load();
+        }
         App.views.viewport.setActiveItem(App.views.popularcard);
     }
 });
