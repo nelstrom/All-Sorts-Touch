4e44143e008ad9b9258996b3e64db44d44139fe1
Drew Neil <andrew.jr.neil@gmail.com>
2011-04-05 17:08:31 +0200

    Move nouns toolbar into its own file.

--- public/app/views/nouns/packet.js
+++ public/app/views/nouns/packet.js
@@ -1,94 +1,17 @@
-App.views.nouns_toolbar = Ext.extend(Ext.Toolbar, {
-    title: "Nouns",
-    items: [
-        {
-            id: 'back_button',
-            text: 'back',
-            ui: 'back',
-            hidden: true,
-            handler: function() {
-                window.history.back();
-            }
-        },
-        {xtype: 'spacer'},
-        {
-            xtype: 'segmentedbutton',
-            items: [
-                {
-                    id: 'prev_noun_letter_button',
-                    iconCls: 'arrow_up',
-                    iconMask: true
-                },
-                {
-                    id: 'next_noun_letter_button',
-                    iconCls: 'arrow_down',
-                    iconMask: true
-                }
-            ]
-        }
-    ],
-    showBackButton: function() {
-        var prevButton = Ext.getCmp("prev_noun_letter_button"),
-            nextButton = Ext.getCmp("next_noun_letter_button"),
-            backButton = Ext.getCmp("back_button");
-
-        prevButton.hide();
-        nextButton.hide();
-        backButton.show();
-    },
-
-    setupScrollerButtons: function(letter) {
-        var prevButton = Ext.getCmp("prev_noun_letter_button"),
-            nextButton = Ext.getCmp("next_noun_letter_button"),
-            backButton = Ext.getCmp("back_button"),
-            alphabet = "abcdefghijklmnopqrstuvwxyz",
-            prevLetterIndex, nextLetterIndex, currentLetterIndex;
-
-        currentLetterIndex = alphabet.indexOf(letter);
-        prevLetterIndex = currentLetterIndex - 1;
-        nextLetterIndex = currentLetterIndex + 1;
-
-        prevButton.show();
-        nextButton.show();
-        backButton.hide();
-
-        prevButton.handler = function() {
-            Ext.dispatch({
-                controller: "nouns",
-                action    : "index",
-                letter    : alphabet[prevLetterIndex],
-                historyUrl: "nouns/by/" + alphabet[prevLetterIndex]
-            });
-        }
-        nextButton.handler = function() {
-            Ext.dispatch({
-                controller: "nouns",
-                action    : "index",
-                letter    : alphabet[nextLetterIndex],
-                historyUrl: "nouns/by/" + alphabet[nextLetterIndex]
-            });
-        }
-    }
-});
-Ext.reg('nounscardtoolbar', App.views.nouns_toolbar);
-
 App.views.NounsPacket = Ext.extend(Ext.Panel, {
     title: "nouns",
     iconCls: "datum",
-    layout: "card",
-    initComponent: function() {
-        Ext.apply(this, {
-            dockedItems: [
-                { xtype: 'nounscardtoolbar', id: 'nouns_toolbar' }
-            ],
-            items: [
-                { xtype: 'nouns_list', id: 'nouns_list' },
-                { xtype: 'noun_detail', id: 'noun_detail' },
-            ]
-        });
-        App.views.NounsPacket.superclass.initComponent.apply(this, arguments);
-    }
-
+    layout: {
+        type: "card",
+        align: "stretch",
+    },
+    dockedItems: [
+        { xtype: 'nounscardtoolbar', id: 'nouns_toolbar' }
+    ],
+    items: [
+        { xtype: 'nouns_list', id: 'nouns_list' },
+        { xtype: 'noun_detail', id: 'noun_detail' },
+    ]
 });
 
 Ext.reg('nouns_packet', App.views.NounsPacket);
